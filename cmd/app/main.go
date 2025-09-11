160d3bc1af8e0a5ee17c459c1dce20242de9d2a6
DavDanielDav <danpicp@gmail.com>
2025-09-11 20:13:49 -0300

    Alteraçao-SayHello

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,14 +2,15 @@
 // Convenção de mercado: colocar em cmd/<nome-app>/main.go
 package main
 
-// Importa os pacotes necessários 
+// Importa os pacotes necessários
 import (
-    "fmt"
-    "github.com/seu-usuario/meu-projeto-go/internal/hello"
+	"fmt"
+
+	"github.com/seu-usuario/meu-projeto-go/internal/hello"
 )
 
 // Função principal do programa
 func main() {
-    fmt.Println("🚀 Meu primeiro projeto em Go com estrutura de mercado!")
-    hello.SayHello()
+	fmt.Println("Minha Primeira alteração em Go!!!")
+	hello.SayHello()
 }
